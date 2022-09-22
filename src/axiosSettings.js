21a7790f1fb07a55265e8f9e5b6cd2c8f1a3d8a7
ValeryGusso <valery.gusso@yandex.ru>
2022-09-22 23:24:04 +0300

    one more fix

--- src/axiosSettings.js
+++ src/axiosSettings.js
@@ -3,7 +3,7 @@ import ax from 'axios'
 const axios = () => {
 	const token = localStorage.getItem('token')
 	return ax.create({
-		baseURL: REACT_APP_API_URL,
+		baseURL: process.env.REACT_APP_API_URL,
 		headers: { authorization: token },
 	})
 }
